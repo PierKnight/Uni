6464c96c5bcef94310df7647471ce5fa2ac1c42f
PierKnight <pier.altimari@libero.it>
2022-06-15 12:15:27 +0200

    big update

---
+++ algoritmi_paralleli/mpi/progetto/allegro/wa-tor-parallelo/parallel.cpp
@@ -0,0 +1,172 @@
+#include <mpi.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include "sequential.cpp"
+#define CELL_SIZE 10
+
+
+
+//compilazione g++ <file> -lallegro
+//g++ parallel.cpp $(pkg-config --libs allegro-5 allegro_main-5 allegro_primitives-5)
+//mpiCC parallel.cpp $(pkg-config --libs allegro-5 allegro_main-5 allegro_primitives-5 )
+//export DISPLAY=:0
+
+
+
+inline void createCreatureType(MPI_Datatype* data)
+{
+    MPI_Aint displacements[3]  = {offsetof(creature,moved),offsetof(creature,energy),offsetof(creature,type)};
+    int block_lengths[3]  = {1,1,1};
+    MPI_Datatype types[3] = {MPI_CXX_BOOL,MPI_INT,MPI_INT};
+    MPI_Type_create_struct(3, block_lengths, displacements, types, data);
+    MPI_Type_commit(data);
+}
+
+inline void drawWorld(creature* matrix)
+{
+    int totalFish = 0;
+    int totalShark = 0;
+    al_clear_to_color(al_map_rgb(0,0,200));
+    al_hold_bitmap_drawing(true);
+    for(int i = 0;i < ROWS;i++)
+    {
+        for(int j = 0;j < COLS;j++)
+        {
+            int index = V(i,j);
+
+            ALLEGRO_COLOR color;
+
+            int type = matrix[index].type;
+            if(type == FISH)
+            {
+                color = al_map_rgb(200,0,0);
+                al_draw_filled_rectangle(j * CELL_SIZE,i * CELL_SIZE,j * CELL_SIZE + CELL_SIZE,i * CELL_SIZE + CELL_SIZE,color);
+                totalFish++;
+            }
+            else if(type == SHARK)
+            {
+                color = al_map_rgb(0,200,0);
+                al_draw_filled_rectangle(j * CELL_SIZE,i * CELL_SIZE,j * CELL_SIZE + CELL_SIZE,i * CELL_SIZE + CELL_SIZE,color);
+                totalShark++;
+            }
+            if(matrix[index].moved)
+                printf("OH NO! %d, %d\n",i,j);
+        }
+    }
+    printf("TOTALE PESCI %d\n",totalFish);
+    printf("TOTALE SQUALI %d\n",totalShark);
+    al_hold_bitmap_drawing(false);
+    al_flip_display();
+}
+
+int main(int argc, char *argv[])
+{
+
+
+    MPI_Init(&argc,&argv);
+    int rank,nProc;
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+    MPI_Comm_size(MPI_COMM_WORLD,&nProc);
+
+    MPI_Datatype type;
+    createCreatureType(&type);
+
+    //questo serve per distrubuire il resto delle righe (se presenti)
+    int extra = (rank < ROWS % nProc) ? 1 : 0;
+    creature* matrix = new creature[(ROWS / nProc + 4 + extra) * COLS];
+
+    creature* drawMatrix;
+    //this two arrays are used to define the size and location of the process array during the gatherv operation
+    int* recvcounts;
+    int* disp;
+    double startTime;
+
+    //setup process 0
+    ALLEGRO_DISPLAY* display;
+    if(rank == 0)
+    {
+        printf("SETUP ALLEGRO\n");
+        if(!al_init())
+            printf("FUCK!\n");
+        display = al_create_display(COLS * CELL_SIZE,ROWS * CELL_SIZE);
+        if(!display)
+            printf("failed to create display!\n");
+        
+        if(al_init_primitives_addon())
+            printf("SETUP DONE\n");
+        else
+            printf("SETUP NOT DONE\n");
+
+
+        drawMatrix = new creature[ROWS * COLS];
+        matrix[V(2,0)].type = FISH;
+
+        recvcounts = new int[nProc];
+        disp = new int[nProc];
+        int spacing = 0;
+        for(int i = 0;i < nProc;i++)
+        {
+            recvcounts[i] = (ROWS / nProc + (i < (ROWS % nProc) ? 1 : 0)) * COLS;
+            disp[i] = spacing;
+            spacing += recvcounts[i];
+        }
+        startTime = MPI_Wtime();
+            
+    }
+
+    for(int t = 0;t < 1000;t++)
+    {
+        //aggiorna l'interno della matrice
+        updateWorld(matrix,3,ROWS / nProc - 2 + extra,true);
+
+
+        MPI_Request request;
+        if(rank % 2 == 0) //i processi pari ricevono i bordi, li aggiornano e li rinviano
+        {
+            MPI_Recv(matrix,COLS * 2,type,(rank + nProc - 1) % nProc,1,MPI_COMM_WORLD,nullptr);
+            MPI_Recv(&matrix[V(ROWS / nProc + 2 + extra,0)],COLS * 2,type,(rank + nProc + 1) % nProc,0,MPI_COMM_WORLD,nullptr);
+
+            updateWorld(matrix, 1, 2,false);
+            updateWorld(matrix, ROWS / nProc + 1 + extra, 2,false);
+
+            MPI_Isend(matrix,2 * COLS,type,(rank + nProc - 1) % nProc,0,MPI_COMM_WORLD,&request);
+            MPI_Isend(&matrix[V(ROWS / nProc + 2 + extra,0)],2 * COLS,type,(rank + nProc + 1) % nProc,1,MPI_COMM_WORLD,&request);
+            
+        }
+        else //i processi dispari inviano i propri bordi e li ricevono
+        {
+            //invio i miei bordi con annesso vicino 
+            MPI_Isend(&matrix[V(2,0)],2 * COLS,type,(rank + nProc - 1) % nProc,0,MPI_COMM_WORLD,&request);
+            MPI_Isend(&matrix[V(ROWS / nProc + extra,0)],2 * COLS,type,(rank + nProc + 1) % nProc,1,MPI_COMM_WORLD,&request);
+            
+            MPI_Recv(&matrix[V(2,0)],COLS * 2,type,(rank + nProc - 1) % nProc,1,MPI_COMM_WORLD,nullptr);
+            MPI_Recv(&matrix[V(ROWS / nProc + extra,0)],COLS * 2,type,(rank + nProc + 1) % nProc,0,MPI_COMM_WORLD,nullptr);
+        }
+
+        
+        MPI_Gatherv(&matrix[V(2,0)],(ROWS / nProc + extra) * COLS,type,drawMatrix,recvcounts,disp,type,0,MPI_COMM_WORLD);
+        if(rank == 0)
+        {
+            printf("DRAW\n");
+            drawWorld(drawMatrix);
+            al_rest(0.1);
+        }
+    
+    }
+
+
+
+    if(rank == 0)
+    {
+        printf("TOTAL TIME %f\n",MPI_Wtime() - startTime);
+        al_destroy_display(display);
+        delete [] drawMatrix;
+        delete [] recvcounts;
+        delete [] disp;
+    }
+
+    delete [] matrix;
+    MPI_Finalize();
+    
+    return 0;
+}
--- algoritmi_paralleli/mpi/progetto/allegro/wa-tor-parallelo/sequential.cpp
+++ algoritmi_paralleli/mpi/progetto/allegro/wa-tor-parallelo/sequential.cpp
@@ -2,17 +2,15 @@
 #define SEQUENTIAL_WATOR
 #include "creature.cpp"
 #include <stdio.h>
-#define ROWS 800
-#define COLS 800
+#define ROWS 64
+#define COLS 64
 #define V(R,C) ((R) * COLS + (C))
-#define fish(F) F > 0 && F < 10000 
-#define shark(F) F >= 10000 
 //fish stats
 #define FISH_TRESHHOLD 1
 //shark stats
-#define START_ENERGY 4
+#define START_ENERGY 50
 #define SHARK_TRESHHOLD 10
-#define EAT_ENERGY 3
+#define EAT_ENERGY 1
 
 inline void getValidPosition(creature* matrix,int i,int j,int type,int* result)
 {
@@ -37,18 +35,13 @@ inline void getValidPosition(creature* matrix,int i,int j,int type,int* result)
     (*result) = valid_pos[rand() % size];
 }
 
-inline bool isBorder(int posRow)
-{
-    return posRow % (ROWS / 2) == 0 || (posRow + 1) % (ROWS / 2) == 0;
-}
-
-inline void fishUpdate(creature* matrix,int i,int j)
+inline void fishUpdate(creature* matrix,int i,int j,int fromRow,int toRow,bool isBorder)
 {
     int pos;
     getValidPosition(matrix,i,j,WATER,&pos);
 
-    //pos = V((i + ROWS + 1) % ROWS,j);
-    //pos = V(i,(j + COLS - 1) % COLS);
+    //pos = V((i + ROWS - 1) % ROWS,j);
+    //pos = V(i,(j + COLS + 1) % COLS);
 
 
     int energy = matrix[V(i,j)].energy;
@@ -63,14 +56,13 @@ inline void fishUpdate(creature* matrix,int i,int j)
     matrix[pos].energy = energy + 1;
     matrix[pos].type = FISH;
 
-    bool test = pos > V(i,j);
 
-    matrix[pos].moved = test;
+    int posRow = pos / COLS;
+    matrix[pos].moved = posRow >= fromRow && posRow < toRow ? pos > V(i,j) : isBorder;
 }
 
-inline void sharkUpdate(creature* matrix,int i,int j)
+inline void sharkUpdate(creature* matrix,int i,int j,int fromRow,int toRow,bool isBorder)
 {
-    return;
     int currentPos = V(i,j);
     int energy = matrix[currentPos].energy;
     //if not enough energy the shark dies
@@ -88,13 +80,15 @@ inline void sharkUpdate(creature* matrix,int i,int j)
     if(pos == currentPos)
     {
         getValidPosition(matrix,i,j,WATER,&pos);
+        int posRow = pos / COLS;
         matrix[currentPos].type = WATER;
         matrix[currentPos].energy = 0;
         matrix[pos].energy = energy - 1;
         matrix[pos].type = SHARK;
-        matrix[pos].moved = pos > currentPos;
+        matrix[pos].moved = posRow >= fromRow && posRow < toRow ? pos > V(i,j) : isBorder;
         return;
     }
+    int posRow = pos / COLS;
 
     //breed if shark has enough energy
     if(energy > SHARK_TRESHHOLD)
@@ -112,27 +106,26 @@ inline void sharkUpdate(creature* matrix,int i,int j)
     //move to fish cell and increase energy
     matrix[pos].energy = energy + EAT_ENERGY;
     matrix[pos].type = SHARK;
-    matrix[pos].moved = pos > currentPos;
+    matrix[pos].moved = posRow >= fromRow && posRow < toRow ? pos > V(i,j) : isBorder;
 }
 
-inline void updateWorld(creature* matrix,int fromRow,int toRow)
+inline void updateWorld(creature* matrix,int fromRow,int rows,bool isBorder)
 {
+    srand (time(NULL));
+
+    
+    int toRow = fromRow + rows;
     for(int i = fromRow;i < toRow;i++)
     {
-
-        //int val = i == -1 ? ROWS - 1 : i;
-
-        //printf("VAL %d\n",val);
-
         for(int j = 0;j < COLS;j++)
         {
             if(!matrix[V(i,j)].moved)
             {
                 creature c = matrix[V(i,j)];
                 if(c.type == FISH)
-                    fishUpdate(matrix,i,j);
+                    fishUpdate(matrix,i,j,fromRow,toRow,isBorder);
                 else if(c.type == SHARK)
-                    sharkUpdate(matrix,i,j);
+                    sharkUpdate(matrix,i,j,fromRow,toRow,isBorder);
             }
             else
                 matrix[V(i,j)].moved = false;
